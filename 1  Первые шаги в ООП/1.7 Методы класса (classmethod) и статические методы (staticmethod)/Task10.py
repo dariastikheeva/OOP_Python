84c7ed5c101b403f66d085cd1e7ecb7af086a39c
Daria Stikheeva <dstikheeva@yandex.ru>
2024-10-16 14:42:53 +0300

    add solution of task 1.7.10

---
+++ 1  Первые шаги в ООП/1.7 Методы класса (classmethod) и статические методы (staticmethod)/Task10.py
@@ -0,0 +1,19 @@
+class AppStore:
+    apps = []
+
+    def add_application(self, app):
+        self.apps.append(app)
+    
+    def remove_application(self, app):
+        self.apps.remove(app)
+
+    def block_application(self, app):
+        app.blocked = True
+
+    def total_apps(self):
+        return len(self.apps)
+    
+class Application:
+    def __init__(self, name):
+        self.name = name
+        self.blocked = False
