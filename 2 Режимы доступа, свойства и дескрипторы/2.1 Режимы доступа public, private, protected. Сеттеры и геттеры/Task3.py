c6c73cf42c8ff873a3a01dd938781bbae00b0827
Daria Stikheeva <dstikheeva@yandex.ru>
2024-10-18 14:02:10 +0300

    add solution of task 2.1.3

---
+++ 2 Режимы доступа, свойства и дескрипторы/2.1 Режимы доступа public, private, protected. Сеттеры и геттеры/Task3.py
@@ -0,0 +1,19 @@
+class Clock:
+    def __init__(self, time):
+        self.__time = 0
+        if self.__check_time(time):
+            self.__time = time
+
+    def set_time(self, tm):
+        if self.__check_time(tm):
+            self.__time = tm
+
+    def get_time(self):
+        return self.time
+    
+    @classmethod
+    def __check_time(cls, tm):
+        return type(tm) is int and 0 <= tm < 100000
+    
+clock = Clock(0)
+clock.set_time(4530)
