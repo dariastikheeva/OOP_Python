1c8d215a51b377dd0c51b55af9f8c8b156434e4f
Daria Stikheeva <dstikheeva@yandex.ru>
2024-11-01 13:34:02 +0300

    add solution of task 3.2.7

---
+++ 3 Магические методы классов/3.2 Метод __call__. Функторы и классы-декораторы/Task7.py
@@ -0,0 +1,15 @@
+from typing import Any
+
+
+class HandlerGET:
+    def __init__(self, func):
+        self.__fn = func
+
+    def __call__(self, request):
+        if request.get('method', 'GET') == 'GET':
+            return self.get(self.__fn, request)
+        else:
+            return None
+        
+    def get(self, func, request, *args, **kwargs):
+        return f'GET: {func(request)}'
