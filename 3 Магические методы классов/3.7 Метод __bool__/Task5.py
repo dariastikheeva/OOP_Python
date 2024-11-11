1cd4ccf04b7cea417498ecf3bc5d777aac52c3cd
Daria Stikheeva <dstikheeva@yandex.ru>
2024-11-11 15:11:05 +0300

    Add solution of task 3.7.5

---
+++ 3 Магические методы классов/3.7 Метод __bool__/Task5.py
@@ -0,0 +1,31 @@
+class MailItem:
+    def __init__(self, mail_from, title, content):
+        self.mail_from = mail_from
+        self.title = title
+        self.content = content
+        self.is_read = False
+
+    def set_read(self, fl_read):
+        self.is_read = fl_read
+
+    def __bool__(self):
+        return self.is_read
+
+class MailBox:
+    def __init__(self):
+        self.inbox_list = []
+
+    def receive(self):
+        lst_in = list(map(str.strip, sys.stdin.readlines()))
+        
+        for mail in lst_in:
+            mail = mail.split(';')
+            item = MailItem(mail[0], mail[1], mail[2])
+            self.inbox_list.append(item)
+
+mail = MailBox()
+mail.receive()
+mail.inbox_list[0].set_read(True)
+mail.inbox_list[-1].set_read(True)
+
+inbox_list_filtered = filter(bool, mail.inbox_list)
