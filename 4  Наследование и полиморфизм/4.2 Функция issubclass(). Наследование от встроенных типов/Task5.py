588588941fb9e88bd6c8a56e1d0717ca246a56d3
Daria Stikheeva <dstikheeva@yandex.ru>
2024-11-25 14:47:49 +0300

    add solution of 4.2.5

---
+++ 4  Наследование и полиморфизм/4.2 Функция issubclass(). Наследование от встроенных типов/Task5.py
@@ -0,0 +1,63 @@
+class Protists:
+    pass
+
+class Plants(Protists):
+    pass
+
+class Animals(Protists):
+    pass
+
+class Mosses(Plants):
+    pass
+
+class Flowering(Plants):
+    pass
+
+class Worms(Animals):
+    pass
+
+class Mammals(Animals):
+    pass
+
+class Human(Mammals):
+    pass
+
+class Monkeys(Mammals):
+    pass
+
+class Monkey(Monkeys):
+    def __init__(self, name, weight, old):
+        super().__init__()
+        self.name = name
+        self.weight = weight
+        self.old = old
+
+class Person(Human):
+    def __init__(self, name, weight, old):
+        super().__init__()
+        self.name = name
+        self.weight = weight
+        self.old = old
+
+class Flower(Flowering):
+    def __init__(self, name, weight, old):
+        super().__init__()
+        self.name = name
+        self.weight = weight
+        self.old = old
+
+class Worm(Worms):
+    def __init__(self, name, weight, old):
+        super().__init__()
+        self.name = name
+        self.weight = weight
+        self.old = old
+
+lst_objs = [Monkey("мартышка", 30.4, 7), Monkey("шимпанзе", 24.6, 8),
+            Person("Балакирев", 88, 34), Person("Верховный жрец", 67.5, 45),
+            Flower("Тюльпан", 0.2, 1), Flower("Роза", 0.1, 2),
+            Worm("червь", 0.01, 1), Worm("червь 2", 0.02, 1)]
+
+lst_animals = [i for i in lst_objs if isinstance(i, Animals)]
+lst_plants = [i for i in lst_objs if isinstance(i, Plants)]
+lst_mammals = [i for i in lst_objs if isinstance(i, Mammals)]
